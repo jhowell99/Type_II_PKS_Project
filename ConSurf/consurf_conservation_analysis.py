31285d63c42b905da29afad79f7c21f1248f29f0
jhowell99 <91912451+jhowell99@users.noreply.github.com>
2022-09-19 10:14:03 +0100

    Adding data

---
+++ ConSurf/consurf_conservation_analysis.py
@@ -0,0 +1,134 @@
+import pandas as pd
+
+eight_all_clades_csv = pd.read_csv(
+    "C16_all_clade_actinorhodin_ConSurf/grades_altered.txt", sep="\t"
+)
+eight_single_clade_csv = pd.read_csv(
+    "C16_clade_actinorhodin_ConSurf/grades_altered.txt", sep="\t"
+)
+nine_ten_all_clades_csv = pd.read_csv(
+    "C18_20_all_clades_warkmycin_ConSurf/grades_altered.txt", sep="\t"
+)
+nine_ten_single_clade_csv = pd.read_csv(
+    "C18_20_clade_warkmycin_ConSurf/grades_altered.txt", sep="\t"
+)
+twelve_thirteen_all_clades_csv = pd.read_csv(
+    "C24_26_all_clades_enduracylinone_ConSurf/grades_altered.txt", sep="\t"
+)
+twelve_thirteen_single_clade_csv = pd.read_csv(
+    "C24_26_clade_enduracyclinone_ConSurf/grades_altered.txt", sep="\t"
+)
+
+
+def colour_score_difference_func(single_df, all_df, text_file_name):
+    single_df["color_score_difference"] = single_df["COLOR"] - all_df["COLOR"]
+
+    resi_number = []
+
+    for i1 in range(0, len(single_df)):
+        res_string = str(single_df.iloc[i1, 0])
+
+        resi_number.append(res_string)
+
+    color_diff_number = []
+
+    for i3 in range(0, len(single_df)):
+        color_string = str(single_df.loc[i3, "color_score_difference"])
+
+        color_diff_number.append(color_string)
+
+    path = text_file_name
+
+    with open(path, "w") as file:
+        for i2 in range(0, len(resi_number)):
+            if color_diff_number[i2] == "8":
+                color = "nothing1"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "7":
+                color = "nothing2"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "6":
+                color = "nothing3"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "5":
+                color = "nothing4"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "4":
+                color = "nothing5"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "3":
+                color = "nothing6"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "2":
+                color = "nothing7"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "1":
+                color = "nothing8"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "0":
+                color = "nothing9"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-1":
+                color = "nothing10"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-2":
+                color = "nothing11"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-3":
+                color = "nothing12"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-4":
+                color = "nothing13"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-5":
+                color = "nothing14"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-6":
+                color = "nothing15"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-7":
+                color = "nothing16"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+            elif color_diff_number[i2] == "-8":
+                color = "nothing17"
+                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("color " + color + ", sele" + "\n")
+
+
+clade_list_single = [
+    eight_single_clade_csv,
+    nine_ten_single_clade_csv,
+    twelve_thirteen_single_clade_csv,
+]
+clade_list_all = [
+    eight_all_clades_csv,
+    nine_ten_all_clades_csv,
+    twelve_thirteen_all_clades_csv,
+]
+output_name = [
+    "eight_clade_color.txt",
+    "nine_ten_clade_color.txt",
+    "twelve_thirteen_clade_color.txt",
+]
+
+
+for i in range(0, 3):
+    colour_score_difference_func(
+        clade_list_single[i], clade_list_all[i], output_name[i]
+    )
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Type_II_PKS_Project
  
+1. AlphaFold_models
+  + The AlphaFold outputs for the three chosen sequences as well as the 6 sequence mutated models
+  + For Model 1-6, there are additional files containing the structures with altered rotamers
+2. CASTp
+  + The folders contain [CASTp](http://sts.bioe.uic.edu/castp/calculation.html) data, all using 1.1 as the radius
+  + There is also a jupyter notebook along with two .csv files to generate some plots
+3. Chen_et_al_SI.xlsx
+  + The SI from [here](https://onlinelibrary.wiley.com/doi/10.1002/anie.202202286)
+  + Contains data for all type II PKS with known sequences and products (as of 6/4/22)
+4. ConSurf
+  + Contains ConSurf runs for the three clades, as well as for the entire dataset, each with the correct reference sequence
+  + To visualise colours,
