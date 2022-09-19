4c6469aca5f0cdec709ddfcea30ae2431e69bbfd
jhowell99 <91912451+jhowell99@users.noreply.github.com>
2022-09-19 13:52:27 +0100

    Update

--- ConSurf/consurf_conservation_analysis.py
+++ ConSurf/consurf_conservation_analysis.py
@@ -43,71 +43,71 @@ def colour_score_difference_func(single_df, all_df, text_file_name):
         for i2 in range(0, len(resi_number)):
             if color_diff_number[i2] == "8":
                 color = "nothing1"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "7":
                 color = "nothing2"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "6":
                 color = "nothing3"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "5":
                 color = "nothing4"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "4":
                 color = "nothing5"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "3":
                 color = "nothing6"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "2":
                 color = "nothing7"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "1":
                 color = "nothing8"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "0":
                 color = "nothing9"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-1":
                 color = "nothing10"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-2":
                 color = "nothing11"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-3":
                 color = "nothing12"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-4":
                 color = "nothing13"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-5":
                 color = "nothing14"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-6":
                 color = "nothing15"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-7":
                 color = "nothing16"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
             elif color_diff_number[i2] == "-8":
                 color = "nothing17"
-                file.write("sele chain B and resi " + resi_number[i2] + "\n")
+                file.write("sele chain C and resi " + resi_number[i2] + "\n")
                 file.write("color " + color + ", sele" + "\n")
 
 
